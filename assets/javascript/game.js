008a39054d262514bb1e4e0b3a50168a3f3dc610
boc4life <lheandrew@gmail.com>
2018-11-13 22:37:58 -0500

    end tuesday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,30 @@
-var words = ["Embiid", "Simmons", "Hoskins", "Wentz"];
+let words = ["embiid", "simmons", "hoskins", "wentz"];
 
-var word = words[Math.floor(Math.random() * words.length)];
+let word = words[Math.floor(Math.random() * words.length)];
+let remainingLetters = word.length
 
-var answerArray = [];
+let answerArray = [];
 for (let i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
+let wrongGuess = [];
 
-document.getElementById("wordboard").innerHTML = answerArray;
+document.getElementById("wordboard").innerHTML = answerArray.join(" ");
+
+document.addEventListener('keypress', function(event) {
+    let guess = event.key;
+    for (let b = 0; b < word.length; b++) {
+        if (word[b] == guess) {
+            answerArray[b] = guess;
+            remainingLetters--;
+            document.getElementById("wordboard").innerHTML = answerArray.join(" ");
+        }
+        
+    }
+})
+
+
+
+console.log(word)
+console.log(answerArray)
+console.log(word[1])
