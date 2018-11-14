f98194c7292e3417a56f2dab0a3ef4887c1c70e2
boc4life <lheandrew@gmail.com>
2018-11-14 14:36:44 -0500

    guessing letters and display ironed out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ let words = ["embiid", "simmons", "hoskins", "wentz"];
 
 let word = words[Math.floor(Math.random() * words.length)];
 let remainingLetters = word.length
+let remainingGuesses = 6;
 
 let answerArray = [];
 for (let i = 0; i < word.length; i++) {
@@ -14,13 +15,22 @@ document.getElementById("wordboard").innerHTML = answerArray.join(" ");
 document.addEventListener('keypress', function(event) {
     let guess = event.key;
     for (let b = 0; b < word.length; b++) {
-        if (word[b] == guess) {
+        if (word[b] === guess) {
             answerArray[b] = guess;
             remainingLetters--;
             document.getElementById("wordboard").innerHTML = answerArray.join(" ");
         }
-        
-    }
+}
+        if (wrongGuess.includes(event.key)) {
+            alert("Already Guessed!");    
+        }
+        else if (answerArray.includes(guess)) {
+        }
+        else {
+            wrongGuess.push(guess);
+            remainingGuesses--;
+            document.getElementById("wrong").innerHTML = wrongGuess.join(" ").toUpperCase();
+        }
 })
 
 
